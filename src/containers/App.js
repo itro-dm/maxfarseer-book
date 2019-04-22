37d74a251d9bce51593d9b995fc1ae83315aa40a
itro-dm <dmitr.morar@gmail.com>
2019-04-22 23:25:09 +0300

    Add presentational and container components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+
+import { User } from '../components/User'
+import { Page } from '../components/Page'
+
 import './App.css'
 
 class App extends Component {
   render() {
-      const { page, user } = this.props;
+    const { page, user } = this.props;
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Мой топ фото</h1>
         </header>
-        <p>Привет: {user.name}</p>
-          <p>У тебя фоток: {page.photos.length} за {page.year} год</p>
+        <User name={user.name} />
+        <Page photos={page.photos} year={page.year} />
       </div>
     )
   }
 }
 
 const mapStateToProps = store => {
-    return {
-        ...store
-    }
+  return {
+    ...store
+  }
 };
-
-
 
 export default connect(mapStateToProps)(App)
--- src/reducers/page.js
+++ src/reducers/page.js
@@ -1,8 +1,13 @@
 const initialState = {
-    year: 2018,
-    photos: [],
+  year: 2018,
+  photos: [],
 };
 
-export function pageReducer(state = initialState) {
-    return state
+export function pageReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'SET_YEAR':
+      return {...state, year: action.payload};
+    default:
+      return state
+  }
 }
